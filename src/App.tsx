896814a2d6f149ab3ea9e0166aac16de5b65ef9d
ahmaidi <ahmaidi@student.1337.ma>
2023-08-02 12:23:40 +0100

    add profile template

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,19 @@ import LogoBar from './Component/LogoBar'
 import Background from './Component/Background'
 import Login from './Component/Login'
 import Profile from './Component/Profile'
-import { UserProvider, UserContext, UserDataType} from './Component/UserContext'
+import { UserProvider} from './Component/UserContext'
 
 function App() {
-  const userContext = useContext<UserDataType | null>(UserContext);
+  // const userContext = useContext<UserDataType | null>(UserContext);
    return (
     <UserProvider>
     <div className='h-[100vh] flex items-center justify-center'>
       <Background />
       <div className="flex flex-col gap-4 bg-background ring ring-white ring-opacity-10 rounded-lg overflow-hidden xsm:w-[80%] sm:w-[80%] md:w-[90%] 2xl:w-[90%]">
       <LogoBar />
-    {(userContext?.isAuthenticated) ? (
+    {/* {(userContext?.isAuthenticated) ? ( */}
           <Routes>
+            <Route path="/Login" element={<Login />} />
             <Route path="/" element={<Home />} />
             <Route path="/Chat" element={<Chat />}  />
             <Route path="/Play" element={<div><h1 style={{color:'#05AE13'}}>Page Play</h1></div>}  />
@@ -26,9 +27,9 @@ function App() {
             <Route path="/Profile" element={<Profile />}  />
             <Route path="*" element={<div><h1 style={{color:'white', fontSize:'25px'}}>404 Page Not Found </h1></div>}  />
           </Routes>
-            ):
-            (<Login />
-            )}
+            // ):
+            // (<Login />
+            // )}
     </div>
   </div>
   </UserProvider>
