ccfc6e76ae1e132c778a854998911660542aa2d8
TN19N <MostafaAnawawi@gmail.com>
2023-09-04 21:24:01 +0100

    fix: the problem with pong render

--- src/Component/game/Pong.tsx
+++ src/Component/game/Pong.tsx
@@ -5,9 +5,7 @@ import MainCanvas from "./PongTable";
 import { param } from "./PongTypes"; // Assuming the type is named PongProps
 import { useEffect, memo } from "react";
 import { Socket, io } from "socket.io-client";
-
 import { errorMsg } from "../Poperror";
-
 
 const BACK_END = `${process.env.SERVER_HOST}/game`;
 
@@ -15,23 +13,24 @@ const Pong: React.FC<param> = (prop: param) => {
   const soc: Socket = io(BACK_END, {
     withCredentials: true,
   });
+
   useEffect(() => {
     soc.on("error", errorMsg);
     return () => {
       soc.disconnect();
     };
-  }, []);
+  }, [soc]);
 
   const mainPong: MainCanvas = new MainCanvas(prop, soc);
-  const setup = (p5: p5Types, canvasParentRef: Element) =>
-    mainPong.init(p5, canvasParentRef);
+  const setup = (p5: p5Types, canvasParentRef: Element) => mainPong.init(p5, canvasParentRef);
 
   const draw = () => mainPong.update();
   const windowResized = () => mainPong.resize();
 
+  const random = Math.random();
   return (
-    <>
-      <Sketch setup={setup} draw={draw} windowResized={windowResized} />
+    <> 
+      <Sketch key={random} setup={setup} draw={draw} windowResized={windowResized} />
     </>
   );
 };
