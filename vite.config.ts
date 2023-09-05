2058d4beaf1d6782dc39332a4981d870908a35b3
TN19N <MostafaAnawawi@gmail.com>
2023-09-05 21:47:52 +0100

    setup hmr for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+      host: 'localhost',
+      clientPort: 5173,
+    }
+  },
   plugins: [react()],
   define: {
     'process.env': process.env
